0cc6cb098ad604c6d21efcde0d36c8be19d9e58c
Marta Łoskot <loskotmarta@gmail.com>
2024-04-26 14:25:30 +0200

    Styling app

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const thingButton = document.querySelector('thing-button')
+const randomThingTitle = document.querySelector('#random-thing')
+const randomThingDescription = document.querySelector('#description')
