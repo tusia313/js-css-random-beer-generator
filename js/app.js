3827c73fdbdc2779adeede50ddbac1824359e7ff
Marta Łoskot <loskotmarta@gmail.com>
2024-04-26 15:58:42 +0200

    fetching our data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-const thingButton = document.querySelector('thing-button')
+const thingButton = document.querySelector('#thing-button')
 const randomThingTitle = document.querySelector('#random-thing')
 const randomThingDescription = document.querySelector('#description')
+
+function getData() {
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+thingButton.addEventListener('click', getData)
